e62d8e8031d02c8d83aa19811f7cccee0182cfc9
sharyarawan23 <sharyarawan23@gmail.com>
2026-09-22 02:07:52 +0300

    Fix Luxury Spray product image paths

--- src/pages/RoomSprays.tsx
+++ src/pages/RoomSprays.tsx
@@ -11,11 +11,15 @@ const useCards = [
 ];
 
 const sprayImageOverrides: Record<string, string> = {
-  "black-london": "/assets/black-london-new.png",
-  "sweet-rose": "/assets/sweet-rose-new.png",
-  "velvet-night": "/assets/velvet-night-new.png",
-  machmoun: "/assets/machmoun-new.png",
-  "cotton-candy": "/assets/cotton-candy-new.png",
+  "black-london": "/assets/spray-products-final/black-london-new.png",
+  "sweet-rose": "/assets/spray-products-final/sweet-rose-new.png",
+  "velvet-night": "/assets/spray-products-final/velvet-night-new.png",
+  machmoun: "/assets/spray-products-final/machmoun-new.png",
+  "cotton-candy": "/assets/spray-products-final/cotton-candy-new.png",
+  "royal-fruity": "/assets/spray-products-final/royal-fruity-new.png",
+  "wood-bouquet": "/assets/spray-products-final/wood-bouquet-new.png",
+  "my-mood": "/assets/spray-products-final/my-mood-new.png",
+  "blanc-imperial": "/assets/spray-products-final/blanc-imperial-new.png",
 };
 
 export default function RoomSprays() {
